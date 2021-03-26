05210cb1453e6607fc2e8e6fa2d585c42e5ea8f3
Tinop <tinopstar2@gmail.com>
2021-03-26 18:02:11 +0100

    create an array that received a list of messages

---
+++ index.js
@@ -0,0 +1,8 @@
+const messages = ['Act as if what you do makes a difference. It does.', 
+'What you get by achieving your goals is not as important as what you become by achieving your goals.', 
+'It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.',
+'Life is like riding a bicycle. To keep your balance, you must keep moving.',
+'You are never too old to set another goal or to dream a new dream.', 
+'You do not find the happy life. You make it.'];
+
+console.log(messages.length)
